37330c3ed6a5cf634d9a09a20afb52408f8a1ff9
HowardStark <howard@getcoffee.io>
2016-04-29 10:28:05 -0700

    Implemented TimerTask

--- app/src/main/java/io/getcoffee/sightglass/notcrappy/background/PowerService.java
+++ app/src/main/java/io/getcoffee/sightglass/notcrappy/background/PowerService.java
@@ -10,6 +10,8 @@ import android.os.IBinder;
 import android.support.annotation.Nullable;
 import android.util.Log;
 
+import java.util.TimerTask;
+
 import io.getcoffee.sightglass.notcrappy.NotCrappyApplication;
 
 /**
@@ -17,10 +19,16 @@ import io.getcoffee.sightglass.notcrappy.NotCrappyApplication;
  */
 public class PowerService extends Service {
 
+    public enum PowerStatus {
+        CONNECTED,DISCONNECTED;
+    }
+
     public static final String SERVICE_TAG = "PowerService";
 
-    BatteryManager batteryManager;
-    ChargingBroadcastReceiver chargingListener;
+    protected BatteryManager batteryManager;
+    protected ChargingBroadcastReceiver chargingListener;
+    private PowerStatus powerStatus = PowerStatus.DISCONNECTED;
+    private int voltage = -1;
 
     @Override
     public void onCreate() {
@@ -41,10 +49,10 @@ public class PowerService extends Service {
         return null;
     }
 
-
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         startChargingListener();
+        setBatteryManager((BatteryManager) getSystemService(BATTERY_SERVICE));
 
         return START_STICKY_COMPATIBILITY;
     }
@@ -53,23 +61,79 @@ public class PowerService extends Service {
         IntentFilter chargingFilter = new IntentFilter();
         chargingFilter.addAction(Intent.ACTION_POWER_CONNECTED);
         chargingFilter.addAction(Intent.ACTION_POWER_DISCONNECTED);
-        chargingListener = new ChargingBroadcastReceiver();
+        chargingListener = new ChargingBroadcastReceiver(this);
         registerReceiver(chargingListener, chargingFilter);
     }
 
+    public PowerStatus getPowerStatus() {
+        return powerStatus;
+    }
+
+    protected void setPowerStatus(PowerStatus powerStatus) {
+        this.powerStatus = powerStatus;
+    }
+
+    public BatteryManager getBatteryManager() {
+        return batteryManager;
+    }
+
+    protected void setBatteryManager(BatteryManager batteryManager) {
+        this.batteryManager = batteryManager;
+    }
+
+    public int getVoltage() {
+        return voltage;
+    }
+
+    protected void setVoltage(int voltage) {
+        this.voltage = voltage;
+    }
+
     protected static class ChargingBroadcastReceiver extends BroadcastReceiver {
+
+        private PowerService powerService;
+
+        public ChargingBroadcastReceiver(PowerService powerService) {
+            this.powerService = powerService;
+        }
 
         @Override
         public void onReceive(Context context, Intent intent) {
             switch(intent.getAction()) {
                 case Intent.ACTION_POWER_CONNECTED:
+                    powerService.setPowerStatus(PowerStatus.CONNECTED);
                     Log.i(PowerService.SERVICE_TAG, "Got 'Power Connected' broadcast.");
                     break;
                 case Intent.ACTION_POWER_DISCONNECTED:
+                    powerService.setPowerStatus(PowerStatus.CONNECTED);
                     Log.i(PowerService.SERVICE_TAG, "Got 'Power Disconnected' broadcast.");
                     break;
                 default:
                     throw new IllegalStateException("ChargingBroadcastReceiver received unacceptable intent action.");
+            }
+        }
+    }
+
+    protected static class PowerTimerTask extends TimerTask {
+
+        public static final String TIMER_TASK_TAG = "PowerTimerTask";
+
+        private PowerService powerService;
+
+        public PowerTimerTask(PowerService powerService) {
+            this.powerService = powerService;
+        }
+
+        public void run() {
+            IntentFilter filter = new IntentFilter(Intent.ACTION_BATTERY_CHANGED);
+            Intent intent = powerService.registerReceiver(null, filter);
+            try {
+                int voltage = intent.getIntExtra(BatteryManager.EXTRA_VOLTAGE, -1);
+                if (voltage > 0) {
+                    powerService.setVoltage(voltage);
+                }
+            } catch(NullPointerException ex) {
+                Log.wtf(PowerTimerTask.TIMER_TASK_TAG, Log.getStackTraceString(ex));
             }
         }
     }
