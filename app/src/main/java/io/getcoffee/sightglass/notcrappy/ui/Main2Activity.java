0be4c651b6aead1f50ed1ba7c194a46a9dc251be
Luke <ldbenkenobi359@gmail.com>
2016-05-01 14:05:57 -0500

    fixing and moving stuff around

--- app/src/main/java/io/getcoffee/sightglass/notcrappy/ui/Main2Activity.java
+++ app/src/main/java/io/getcoffee/sightglass/notcrappy/ui/Main2Activity.java
@@ -1,4 +1,4 @@
-package io.getcoffee.sightglass.notcrappy;
+package io.getcoffee.sightglass.notcrappy.ui;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -12,6 +12,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+
+import io.getcoffee.sightglass.notcrappy.R;
 
 public class Main2Activity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
